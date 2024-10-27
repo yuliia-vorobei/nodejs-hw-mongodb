8ec5af65f2a6dea7ef0144c55d7cf0901be9bd61
Yuliia Vorobei <vorobeju1@gmail.com>
2024-10-27 21:25:37 +0100

    extra route added

--- src/server.js
+++ src/server.js
@@ -29,6 +29,7 @@ export const setupServer = () => {
     //req.params зберігаються динамічні дані
     const { contactId } = req.params;
     const data = await contactServices.getContactById(contactId);
+
     if (!data) {
       res.status(404).json({
         message: 'Contact not found',
@@ -45,6 +46,12 @@ export const setupServer = () => {
 
   app.use((req, res) => {
     // лише статус 200 є автом, всі інші треба вказувати
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
