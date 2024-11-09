9d5c1bbab9062c8a97164c1c455b97b58ea27a1d
Yuliia Vorobei <vorobeju1@gmail.com>
2024-11-09 15:20:11 +0100

    fixed id validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,11 +8,11 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { isValidId } from '../middlewares/isValidId.js';
 import {
   createContactSchema,
   updateContactSchema,
 } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
