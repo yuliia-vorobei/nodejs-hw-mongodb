a367a2e63b1f41cc89ab6332866c6e1a72f8fbbf
Yuliia Vorobei <vorobeju1@gmail.com>
2024-11-17 18:39:37 +0100

    deleted validation

--- src/validation/session.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-export const userSessionSchema = Joi.object({
-  userId: Joi.string().required(),
-  accessToken: Joi.string().required(),
-  refreshToken: Joi.string().required(),
-  accessTokenValidUntil: Joi.date().required(),
-  refreshTokenValidUntil: Joi.date().required(),
-});
