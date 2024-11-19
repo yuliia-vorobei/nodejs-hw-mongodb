2fd8043c40c4376017fc0c08f299a8576420bf46
Yuliia Vorobei <vorobeju1@gmail.com>
2024-11-19 16:49:33 +0100

    removed env

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,15 +1,15 @@
-import { env } from '../utils/env.js';
-import nodemailer from 'nodemailer';
+// import { env } from '../utils/env.js';
+// import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  host: env('SMTP_HOST'),
-  port: Number(env('SMTP_PORT')),
-  auth: {
-    user: env('SMTP_USER'),
-    pass: env('SMTP_PASSWORD'),
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: env('SMTP_HOST'),
+//   port: Number(env('SMTP_PORT')),
+//   auth: {
+//     user: env('SMTP_USER'),
+//     pass: env('SMTP_PASSWORD'),
+//   },
+// });
 
-export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
-};
+// export const sendEmail = async (options) => {
+//   return await transporter.sendMail(options);
+// };
